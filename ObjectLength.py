7054625580e142e2c0f6bd680c39a0fee7870387
jainishmehta <45054638+jainishmehta@users.noreply.github.com>
2020-07-15 19:06:05 -0400

    Abstracted some functions into another file.

--- ObjectLength.py
+++ ObjectLength.py
@@ -1,23 +1,17 @@
+import cv2
+import numpy as np
 from scipy.spatial.distance import euclidean
 from imutils import perspective
 from imutils import contours
-import numpy as np
 import imutils
-import cv2
 import utlis
 
-# Function to show array of images (intermediate results)
-def show_images(images):
-	for i, img in enumerate(images):
-		cv2.imshow("image_" + str(i), img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
 
-img_path = "hi.jpg"
+#Insert the image path name
+image_name = "foot.jpg"
 
-# Do preprocessing
-image = cv2.imread(img_path)
-
+# Read image and preprocessing
+image = cv2.imread(image_name)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (9, 9), 0)
 
@@ -25,63 +19,41 @@ edged = cv2.Canny(blur, 50, 100)
 edged = cv2.dilate(edged, None, iterations=1)
 edged = cv2.erode(edged, None, iterations=1)
 
-#show_images
+# Find the # of contours
+count = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+count = imutils.grab_contours(count)
 
-# Find contours
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+# Sort contours from left to right as leftmost contour is reference object
+(count, _) = contours.sort_contours(count)
 
-# Sort contours from left to right 
-(cnts, _) = contours.sort_contours(cnts)
+# Remove small contours, removes noise
+count = [x for x in count if cv2.contourArea(x) > 90]
 
-# Remove contours which are not large enough
-cnts = [x for x in cnts if cv2.contourArea(x) > 100]
+# Get reference object dimensions
+ref_object = count[0]
+surroundings = cv2.minAreaRect(ref_object)
+surroundings = cv2.boxPoints(surroundings )
+surroundings = np.array(surroundings , dtype="int")
+surroundings = perspective.order_points(surroundings )
+(tl, tr, br, bl) = surroundings
+pixel_dist = euclidean(tl, tr)
+#Can adjust this number based on leftmost object size
+dist_in_cm = 2.0
+#Uses pixels for measurement
+pixel_per_cm = pixel_dist/dist_in_cm
 
-# Reference object dimensions
-# Here for reference I have used a 2cm x 2cm square
-ref_object = cnts[0]
-box = cv2.minAreaRect(ref_object)
-box = cv2.boxPoints(box)
-box = np.array(box, dtype="int")
-box = perspective.order_points(box)
-(tl, tr, br, bl) = box
-dist_in_pixel = euclidean(tl, tr)
-dist_in_cm = 2
-pixel_per_cm = dist_in_pixel/dist_in_cm
 
-# Draw remaining contours
-for cnt in cnts:
-	box = cv2.minAreaRect(cnt)
-	box = cv2.boxPoints(box)
-	box = np.array(box, dtype="int")
-	box = perspective.order_points(box)
-	(tl, tr, br, bl) = box
-	cv2.drawContours(image, [box.astype("int")], -1, (0, 0, 255), 2)
-	mid_pt_horizontal = (tl[0] + int(abs(tr[0] - tl[0])/2), tl[1] + int(abs(tr[1] - tl[1])/2))
-	mid_pt_verticle = (tr[0] + int(abs(tr[0] - br[0])/2), tr[1] + int(abs(tr[1] - br[1])/2))
-	wid = euclidean(tl, tr)/pixel_per_cm
-	ht = euclidean(tr, br)/pixel_per_cm
-	cv2.putText(image, "{:.1f}cm".format(wid), (int(mid_pt_horizontal[0] - 15), int(mid_pt_horizontal[1] - 10)),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
-	cv2.putText(image, "{:.1f}cm".format(ht), (int(mid_pt_verticle[0] + 10), int(mid_pt_verticle[1])),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+# Draw all contours that were counted
+utlis.drawCoutour (count, pixel_per_cm, image)
+#Check utlis for definition
+utlis.show_images([image])
 
-show_images([image])
-
-webcam = True
-path = '1.jpg'
+#Can extend for video capture for an extension
 cap = cv2.VideoCapture(0)
-cap.set(10,160)
-cap.set(3,1920)
-cap.set(4,1080)
-scale = 3
-wP = 210 *scale
-hP= 297 *scale
 
 while True:
-    if webcam:success,img = cap.read()
-    else: img = cv2.imread(path)
-img = cv2.resize(img,(0,0),None,0.5,0.5)
-    cv2.imshow('Original',img)
-# The second part can be used for live webcam feed (an extension)
+      ret, frame = cap.read() #returns ret and the frame
+      cv2.imshow('frame',frame)
 
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
