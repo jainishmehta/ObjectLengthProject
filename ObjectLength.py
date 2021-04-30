850b00d78d5b7ec409515530b7d9045931cffb7d
Jainish Mehta <45054638+jainishmehta@users.noreply.github.com>
2021-04-30 02:25:05 -0400

    Recombined into one file

--- ObjectLength.py
+++ ObjectLength.py
@@ -1,11 +1,40 @@
 import cv2
+#This will show the image
 import numpy as np
 from scipy.spatial.distance import euclidean
 from imutils import perspective
 from imutils import contours
 import imutils
-import utils
 
+def drawCoutour (count, pixel_per_cm, image):
+    for cnt in count:
+        surroundings = cv2.minAreaRect(cnt)
+        surroundings = cv2.boxPoints(surroundings )
+        surroundings = np.array(surroundings , dtype="int")
+        surroundings = perspective.order_points(surroundings )
+        (tl, tr, br, bl) = surroundings
+        #Draw the boxes
+        cv2.drawContours(image, [surroundings .astype("int")], -1, (0, 0, 255), 2)
+         
+        #Calculate the midpoints for the vertical and horizontal sections
+        vertical_mid = (tr[0] + int(abs(br[0]-tr[0])/2), tr[1] + int(abs( br[1]-tr[1])/2))
+        horizontal_mid = (tl[0] + int(abs( tl[0]-tr[0] )/2), tl[1] + int(abs( tl[1]-tr[1] )/2))
+            
+        #Calculate the pixel per meter value  
+        width = euclidean(tl, tr)/pixel_per_cm
+        height = euclidean(tr, br)/pixel_per_cm
+            
+         # Put the information as text on the image  
+        cv2.putText(image, "{:.1f}cms".format(height), (int(vertical_mid[0] + 10), int(vertical_mid[1])),
+            cv2.FONT_ITALIC, 0.5, (25, 23, 0), 2)
+        cv2.putText(image, "{:.1f}cms".format(width), (int(horizontal_mid [0] - 15), int(horizontal_mid [1] - 10)),
+            cv2.FONT_ITALIC, 0.5, (25, 23, 0), 2)
+ 
+def show_images(images):
+	for i, image in enumerate(images):
+		cv2.imshow("image ", image)
+		#waitKey(0) will display the window infinitely until any keypress
+	cv2.waitKey(0)
 
 #Insert the image path name
 image_name = "foot.jpg"
@@ -44,9 +73,9 @@ pixel_per_cm = pixel_dist/dist_in_cm
 
 
 # Draw all contours that were counted
-utils.drawCoutour (count, pixel_per_cm, image)
+drawCoutour (count, pixel_per_cm, image)
 #Check utlis for definition
-utils.show_images([image])
+show_images([image])
 
 #Can extend for video capture eventually
 cap = cv2.VideoCapture(0)
